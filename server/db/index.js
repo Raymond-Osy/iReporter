2e9353b2e17b776a7f265ad82a6a20883eaac1c9
Raymond Osy <raymondosy1@gmail.com>
2018-12-12 10:17:39 +0100

    chore(postgreSql): setup database
    - Connect database
    - Add createTables query for creating a table in the database
    - Add dropTables query  for deleting table in the database
    - Add reset-tables command in package.json to reset table in database
    - Add .env file to the .gitIgnore to ensure data protection
    
    [Fixes #162572338]

--- server/db/index.js
+++ server/db/index.js
@@ -1,20 +1,11 @@
-// import dotenv from 'dotenv';
-// import pg from 'pg-promise';
+import dotenv from 'dotenv';
 import { Pool } from 'pg';
+
+dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 });
-
-pool.query(createUserTable)
-  .then((response) => {
-    console.log(response);
-    pool.end();
-  })
-  .catch((error) => {
-    console.log(error);
-    pool.end();
-  });
 
 module.exports = {
   query: (text, params, callback) => pool.query(text, params, callback)
