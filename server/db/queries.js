7edfe10804ece0ff3f42543ee907abf7312f3870
Raymond Osy <raymondosy1@gmail.com>
2018-12-13 16:21:00 +0100

    ft(createIntervention): Add endpoint for creating an intervention
    -Add endpoint
    for creating an intervention
    - Secured it with jwt
    
    [fixes: #162629861]

--- server/db/queries.js
+++ server/db/queries.js
@@ -1,8 +1,10 @@
 const queryUsers = 'SELECT * from users where email = $1 and password = $2';
 
 const insertIntoUsers = 'INSERT INTO users(firstname, lastname, othernames, email, phoneNumber, username, password) values($1, $2, $3, $4, $5, $6, $7) RETURNING *';
+const createIncident = 'INSERT INTO incidents(createdBy, type, location, status, comment) values($1, $2, $3, $4, $5) RETURNING *';
 
 const Queries = {
+  createIncident,
   queryUsers,
   insertIntoUsers
 };
